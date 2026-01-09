5bca31b2fc10f4a5d47b15d0d1079d0395b6a53b
Siak Hooi <siakhooi@gmail.com>
2026-01-09 01:11:11 +0000

    refactor check binaries logic

--- base-fedora/bin/devcontainer-init-shell.sh
+++ base-fedora/bin/devcontainer-init-shell.sh
@@ -13,19 +13,3 @@ for f in /opt/devcontainer/bashrc.d/*.sh; do
 	# shellcheck disable=SC1090
 	. "$f"
 done
-
-commands=(ack awk curl git gpg grep make nano sed sudo tar tree vi wget zip)
-
-found=()
-not_found=()
-# Check if each command is available
-for cmd in "${commands[@]}"; do
-	if ! command -v "$cmd" >/dev/null 2>&1; then
-		not_found+=("$cmd")
-	else
-		found+=("$cmd")
-	fi
-done
-
-[ ${#found[@]} -ne 0 ] && echo "Available: ${found[*]}"
-[ ${#not_found[@]} -ne 0 ] && echo "Not Available: ${not_found[*]}"
