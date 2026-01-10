cad17eb1cafdeae19cb7e11c69d1f6b757e18745
Siak Hooi <siakhooi@gmail.com>
2026-01-10 03:01:47 +0000

    update check-binaries.sh

--- base-fedora/bashrc.d/check-binaries.sh
+++ base-fedora/bashrc.d/check-binaries.sh
@@ -3,11 +3,13 @@
 # Devcontainer check binaries from internal, to be provided by devcontainer
 for f in /etc/devcontainer/check-binaries/*; do
 	[ -e "$f" ] || continue
-	devy-check-binaries -f "$f"
+  echo -n 'bin check: '
+	devy-check-binaries -V -f "$f"
 done
 
 # Devcontainer check binaries from external, to be mounted by user
 for f in /opt/devcontainer/check-binaries/*; do
 	[ -e "$f" ] || continue
-	devy-check-binaries -f "$f"
+  echo -n 'bin check: '
+	devy-check-binaries -V -f "$f"
 done
