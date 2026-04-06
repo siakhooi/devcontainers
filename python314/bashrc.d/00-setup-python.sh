ddf50d6127bbe5806edf71b8be65b6ef52c63b0e
Siak Hooi <siakhooi@gmail.com>
2026-04-06 11:25:38 +0000

    update python global versions

--- python314/bashrc.d/00-setup-python.sh
+++ python314/bashrc.d/00-setup-python.sh
@@ -7,9 +7,6 @@ if command -v pyenv 1>/dev/null 2>&1; then
 	eval "$(pyenv init -)"
 	eval "$(pyenv virtualenv-init -)"
 
-  if [[ ! -f .python-version ]]; then
-    pyenv versions --bare > .python-version
-  fi
 fi
 
 export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
